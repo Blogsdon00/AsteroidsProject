fc884072542ce5dff7a9e109d21ff4788ed258c5
Blogsdon00 <logsdon.bailey00@gmail.com>
2026-02-25 20:13:31 -0800

    Adding new files and changes to existing files to create a player triangle on the screen.

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import pygame
 from constants import *
 from logger import log_state
+from player import Player
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     pygame_ver = pygame.version.ver
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print(f"Starting Asteroids with pygame version: {pygame_ver}")
@@ -14,7 +18,11 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        clock.tick(60)
+        dt = clock.tick(60) / 1000
+        
 
 
 if __name__ == "__main__":
