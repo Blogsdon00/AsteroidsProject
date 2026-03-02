5199559f20581b2194bf9fa4d96288d82fd4af59
Blogsdon00 <logsdon.bailey00@gmail.com>
2026-03-01 21:02:04 -0800

    Added files to create an asteroid field and collision detections. Game ends once player runs into an asteroid.

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
+import sys
 import pygame
 from constants import *
-from logger import log_state
+from logger import log_state, log_event
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 def main():
+    asteroids = pygame.sprite.Group()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Asteroid.containers = (asteroids, updatable, drawable)
+    Player.containers = (updatable, drawable)
+    AsteroidField.containers = (updatable)
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    field = AsteroidField()
     pygame_ver = pygame.version.ver
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print(f"Starting Asteroids with pygame version: {pygame_ver}")
@@ -18,8 +28,14 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player.update(dt) 
-        player.draw(screen)
+        updatable.update(dt) 
+        for obj in drawable:
+            obj.draw(screen)
+        for object in asteroids:
+            if object.collides_with(player):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
         pygame.display.flip()
         clock.tick(60)
         dt = clock.tick(60) / 1000
